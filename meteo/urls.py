67c1e16464eaa5551af5a758bb2e809e4bc8c270
IsmailAnjri <ismailanjri125@gmail.com>
2025-03-19 16:25:39 +0100

    Adding User authentication and authorization.

--- meteo/urls.py
+++ meteo/urls.py
@@ -1,9 +1,23 @@
+from django.conf.urls.static import static
 from django.urls import path
-
 from meteo import views
+
+from django.contrib import admin  # Django admin module
+from authentication.views import *  # Import views from the authentication app
+from django.conf import settings   # Application settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  # Static files serving
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('login/', views.login_page, name='login_page'),    # Login page
+    path('register/', views.register_page, name='register'),  # Registration page
     path("meteo/", views.temp_here, name="temp_here"),
     path("meteo/discover", views.temp_somewhere, name="temp_somewhere")
 ]
+
+# Serve media files if DEBUG is True (development mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files using staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
--- meteo/views.py
+++ meteo/views.py
@@ -5,12 +5,81 @@ import requests
 from django.http import HttpResponse
 from django.template import loader
 from meteo.models import Worldcities
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
 
 
 # Create your views here.
 def home(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
+
+
+def login_page(request):
+    # Check if the HTTP request method is POST (form submission)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Check if a user with the provided username exists
+        if not User.objects.filter(username=username).exists():
+            # Display an error message if the username does not exist
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+
+        # Authenticate the user with the provided username and password
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            # Display an error message if authentication fails (invalid password)
+            messages.error(request, "Invalid Password")
+            return redirect('/login/')
+        else:
+            # Log in the user and redirect to the home page upon successful login
+            login(request, user)
+            return redirect('/')
+
+    # Render the login page template (GET request)
+    return render(request, 'login_page.html')
+
+
+def register_page(request):
+    # Check if the HTTP request method is POST (form submission)
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Check if a user with the provided username already exists
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            # Display an information message if the username is taken
+            messages.info(request, "Username already taken!")
+            return redirect('/register/')
+
+        # Create a new User object with the provided information
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+
+        # Set the user's password and save the user object
+        user.set_password(password)
+        user.save()
+
+        # Display an information message indicating successful account creation
+        messages.info(request, 'Account created Successfully! Please <a href="/login/">click here</a> to login.')
+        return redirect('/register/')
+
+    # Render the registration page template (GET request)
+    return render(request, 'register.html')
 
 def temp_somewhere(request):
     random_item = Worldcities.objects.all().order_by('?').first()
